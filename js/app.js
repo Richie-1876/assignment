f1fa670ad71240de634547ed9beda19eb9e37925
Richard Wright <richardwright@MACBOOKPRO-FF83.station>
2021-08-27 13:24:31 +0100

    added icons for back and forward buttons, and added functionality to them with css, implemented smooth scroll with css, added icon for settings and build settings panel, implememnted javaScript to toggle preferences panel

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log("Connected");
+/////////////////////////////////////////
+//         TOGGLE THE DISPLAY
+//         OF USER PREFERENCES PANEL
+/////////////////////////////////////////
+let panel = document.getElementById("preference-panel");
+
+function togglePreferencePanel() {
+  //get css style declaration object of the current computed styling of the element.
+  var panelStyle = getComputedStyle(panel);
+
+  if (panelStyle.display == "none") {
+    panel.style.display = "block";
+  } else {
+    panel.style.display = "none";
+  }
+}
