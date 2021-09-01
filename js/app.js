10408edb2c0cf18602a00e01a19aa5518a803ea3
Richard Wright <richardwright@MACBOOKPRO-FF83.station>
2021-09-01 12:35:07 +0100

    started to work on the javascript for the preferences panel and implememnted a preferences object for testing and added a button to test the changing of the backgroundcolor of main and the color and fontsie of main

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 /////////////////////////////////////////
+//         GET ELEMENTS
+/////////////////////////////////////////
+let panel = document.getElementById("preference-panel");
+var main = document.getElementById("main-content");
+/////////////////////////////////////////
 //         TOGGLE THE DISPLAY
 //         OF USER PREFERENCES PANEL
 /////////////////////////////////////////
-let panel = document.getElementById("preference-panel");
 
 function togglePreferencePanel() {
   //get css style declaration object of the current computed styling of the element.
@@ -14,3 +18,56 @@ function togglePreferencePanel() {
     panel.style.display = "none";
   }
 }
+/////////////////////////////////////////
+//    Preference object for testing
+/////////////////////////////////////////
+var testPreferences = {
+  backgroundColor: "blue",
+  fontSize: "22px",
+  color: "magenta",
+};
+
+/////////////////////////////////////////
+//    Function to set preferences
+/////////////////////////////////////////
+/****************************************
+ * Name: setPreferences()
+ * Purpose: Set the <main> font size and color, <main> Background-color
+ * Parameters: (1) name: preferences type: Object
+ ****************************************/
+
+function setPreferences(preferences) {
+  // if the key of backgroundColor is present in the object - set the background color to its value
+  if (preferences.backgroundColor != undefined) {
+    main.style.backgroundColor = preferences.backgroundColor;
+  }
+  // Otherwise log "no key of backgroundColor found in preferences object"
+  else {
+    console.log("no key of backgroundColor found in preferences object");
+  }
+  // if the key of color is present in the object - set the color in <main> to its value
+
+  if (preferences.color != undefined) {
+    main.style.color = preferences.color;
+  }
+  // Otherwise log "no key of color found in preferences object"
+  else {
+    console.log("no key of color found in preferences object");
+  }
+  // if the key of fontSize is present in the object - set the fontsize in <main> to its value
+
+  if (preferences.fontSize != undefined) {
+    main.style.fontSize = preferences.fontSize;
+  }
+  // Otherwise log "no key of fontSize found in preferences object"
+  else {
+    console.log("no key of fontSize found in preferences object");
+  }
+}
+
+// call the setPreferences function on click of the test button in the preferences panel
+let testBtn = document
+  .getElementById("test-btn")
+  .addEventListener("click", () => {
+    setPreferences(testPreferences);
+  });
