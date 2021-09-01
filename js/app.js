43bed37e1ec05c8c82d76adbf71e215731787118
Richard Wright <richardwright@MACBOOKPRO-FF83.station>
2021-09-01 16:26:24 +0100

    added functions to write preference objects to local staroage and read preference objects from local storeage, added event listeners on the elements in the preference panel to allow them to be targetted

--- js/app.js
+++ js/app.js
@@ -1,12 +1,40 @@
 /////////////////////////////////////////
 //         GET ELEMENTS
 /////////////////////////////////////////
-let panel = document.getElementById("preference-panel");
+var panel = document.getElementById("preference-panel");
 var main = document.getElementById("main-content");
+
+/////////////////////////////////////////
+//         EVENT LISTENERS
+/////////////////////////////////////////
+document
+  .getElementById("background-color-picker")
+  .addEventListener("change", (e) => {
+    console.log("Background color picker changed", `value: ${e.target.value}`);
+  });
+document.getElementById("font-color-picker").addEventListener("change", (e) => {
+  console.log("Font color picker changed", `value: ${e.target.value}`);
+});
+document.getElementById("font-range-slider").addEventListener("change", (e) => {
+  console.log("Font size value changed", `value: ${e.target.value}`);
+});
+/////////////////////////////////////////
+//      Function to update displayed font
+//      size in preference panel
+/////////////////////////////////////////
+/****************************************
+ *
+ ****************************************/
+
 /////////////////////////////////////////
 //         TOGGLE THE DISPLAY
 //         OF USER PREFERENCES PANEL
 /////////////////////////////////////////
+/****************************************
+ * Name: togglePreferencePanel()
+ * Purpose: toggle between display block an display none
+ * Parameters: (0)
+ ****************************************/
 
 function togglePreferencePanel() {
   //get css style declaration object of the current computed styling of the element.
@@ -64,10 +92,64 @@ function setPreferences(preferences) {
     console.log("no key of fontSize found in preferences object");
   }
 }
-
-// call the setPreferences function on click of the test button in the preferences panel
+/////////////////////////////////////////
+//   Add eventListener to text button
+/////////////////////////////////////////
+/************************************
+ * call the setPreferences function on
+ * click of the test button in the preferences panel
+ *************************************/
 let testBtn = document
   .getElementById("test-btn")
   .addEventListener("click", () => {
     setPreferences(testPreferences);
   });
+////////////////////////////////////////////////
+// Function to save preferences object in local storage
+////////////////////////////////////////////////
+/****************************************
+ * Name: savePreferences()
+ * Purpose:
+ * 1 - create a JSON string of the preferences object
+ * 2 - Save that JSON string and keyName to Local Storage
+ * Parameters: (2) preferences(object), keyName(string)
+ * preferences object should have keys of:
+ * backgroundColor: "VALUE",
+ * fontSize: "VALUE",
+ * color: "VALUE",
+ ****************************************/
+function savePreferences(preferences, keyName) {
+  var preferencesJSONstring = JSON.stringify(preferences);
+  localStorage.setItem(keyName, preferencesJSONstring);
+}
+////////////////////////////////////////////////
+// Function to retrieve preference object from
+// local storage by given key name
+////////////////////////////////////////////////
+/****************************************
+ * Name: getPreferences()
+ * Purpose:
+ *   - get a preferences object from local storage
+ *     by a given keyName.
+ *   - If it exists returns the preferences object.
+ *   - Otherwise return null
+ * Parameters: (1) keyName(string)
+ ****************************************/
+function getPreferences(keyName) {
+  if (localStorage[keyName] != undefined) {
+    console.log(`Preferences Object with keyName ${keyName}`);
+    return JSON.parse(localStorage[keyName]);
+  } else {
+    console.error(
+      `No preference Object with keyName: ${keyName} found in local storage`
+    );
+    return null;
+  }
+}
+
+////////////////////////////////////////////////
+// Test of the savePreferences Function with testPreferences
+////////////////////////////////////////////////
+// savePreferences(testPreferences, "testSettings");
+var fetchedSettings = getPreferences("testSettings");
+console.log(fetchedSettings);
